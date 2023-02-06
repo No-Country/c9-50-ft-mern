a2948993ce95ce0e6d5e0aef425908a98e2859aa
agustinlopez23 <agustinlopez223@gmail.com>
2023-02-06 18:01:28 -0300

    install routerdom, and create foldersstructure

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,20 @@
-const messages = 'Hello World'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import { Home, Login, Recovery, Register } from './Pages'
+
+const messages = 'Hello World!!'
 
 function App() {
   return (
     <div>
-      <h1 className="tracking-widest font-semibold">{messages}</h1>
+      <h1 className='tracking-widest font-semibold'>{messages}</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/recovery' element={<Recovery />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ app/src/Pages/index.js
@@ -0,0 +1,4 @@
+export { Login } from './Login'
+export { Recovery } from './Recovery'
+export { Register } from './Register'
+export { Home } from './Home'
