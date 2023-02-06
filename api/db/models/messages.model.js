eb698e4cf10ec567bb90e0ee12cc99dd7ce473a1
somosa <85375369+CarlosEduardoSomosaMartinez@users.noreply.github.com>
2023-02-06 16:04:11 -0600

    relationShip mongoose

--- api/db/models/messages.model.js
+++ api/db/models/messages.model.js
@@ -2,10 +2,10 @@ const { Schema, model } = require('mongoose')
 
 const messageSchema = new Schema(
   {
-    text: { type: String, required: true },
+    message: { type: String, required: true },
     // lo dejo preparado con String para que queden completos, modificalos para hacer la relacion.
-    senderId: { type: String, required: true },
-    revieverId: { type: String, required: true }
+    senderId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    revieverId: { type: Schema.Types.ObjectId, ref: 'User', required: true }
   },
   {
     timestamps: true
