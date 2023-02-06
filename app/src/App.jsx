bfd2a76f939b1111f33426909797cbd0ef481f3c
JoseLuria <jos02-log18@hotmail.com>
2023-02-06 15:13:36 -0600

    fixed pages imports and added toastify

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,18 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Home, Login, Recovery, Register } from './Pages'
+import { Home, Login, Recovery, Register } from './pages'
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/recovery' element={<Recovery />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/recovery' element={<Recovery />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
