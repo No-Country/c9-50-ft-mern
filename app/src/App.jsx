b3095251bb2e0d26794600e5bb2b08116fd52ae2
JoseLuria <jos02-log18@hotmail.com>
2023-02-06 16:10:52 -0600

    deleted temporal components

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,19 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Home, Login, Recovery, Register } from '@/pages'
 import { ToastContainer } from 'react-toastify'
-import { AxiosInterceptor } from '@/utils'
 import 'react-toastify/dist/ReactToastify.min.css'
-
-AxiosInterceptor()
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/recovery' element={<Recovery />} />
+        <Route path='/' element={<h1>Hello</h1>} />
       </Routes>
       <ToastContainer icon theme='colored' />
     </BrowserRouter>
--- app/src/Components/Form/FormFieldText.jsx
+++
@@ -1,3 +0,0 @@
-export const FormFieldText = () => {
-  return <div>FormFieldText</div>
-}
