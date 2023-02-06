922638e88339b984209205ab8df82b5636188d50
Ivan Passalia <kakaroto11@live.com.ar>
2023-02-06 17:27:23 -0300

    comentaries

--- api/db/models/role.model.js
+++ api/db/models/role.model.js
@@ -2,6 +2,7 @@ const { Schema, model } = require('mongoose')
 
 const roleSchema = new Schema(
   {
+    // Discutir tema admin en la reunion de mañana
     type: { type: String, enum: ['COLABORATOR', 'PATIENT', 'ADMIN'] },
     refered: { type: String },
     meetUrl: { type: String }
