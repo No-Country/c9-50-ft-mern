ffa81aa892cbdcb8478e168fe0d9f0f3f3b071e0
JoseLuria <jos02-log18@hotmail.com>
2023-02-06 13:44:47 -0600

    added user mongoose model

---
+++ api/db/models/user.model.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, require: true, unique: true },
+    password: { type: String, required: true },
+    //TODO: agregar el modelo de rol
+    ocupation: { type: String, required: true },
+    bornDate: { type: Date, requred: true }
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = model('User', userSchema)
