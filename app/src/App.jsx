15986819c96c0c3b681992811f7c43200d9df081
JoseLuria <jos02-log18@hotmail.com>
2023-02-06 16:13:14 -0600

    changed folders names

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home, Login, Recovery, Register } from '@/Pages'
 import { ToastContainer } from 'react-toastify'
+import { AxiosInterceptor } from '@/utils'
 import 'react-toastify/dist/ReactToastify.min.css'
+
+AxiosInterceptor()
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<h1>Hello</h1>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/recovery' element={<Recovery />} />
       </Routes>
       <ToastContainer icon theme='colored' />
     </BrowserRouter>
