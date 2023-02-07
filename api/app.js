a5cdde11c6511663a477012ee10d8d1acb76eb28
JoseLuria <jos02-log18@hotmail.com>
2023-02-07 09:58:04 -0600

    fixed router import problem

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config({})
 const express = require('express')
 const cors = require('cors')
-const { router } = require('./router')
+const router = require('./router')
 const databaseConnect = require('./db')
 
 const PORT = process.env.PORT ?? 3001
--- api/libs/zod/role.schema.js
+++ api/libs/zod/role.schema.js
@@ -1,9 +1,11 @@
 const { z } = require('zod')
 
-const registerRoleSchema = z.object({
-  type: z.enum(['COLABORATOR', 'PATIENT', 'ADMIN']),
-  refered: z.string(),
-  url: z.string().url()
-}).required()
+const registerRoleSchema = z
+  .object({
+    type: z.enum(['COLABORATOR', 'PATIENT', 'ADMIN']),
+    refered: z.string(),
+    url: z.string().url()
+  })
+  .required()
 
-module.exports = registerRoleSchema
+module.exports = { registerRoleSchema }
