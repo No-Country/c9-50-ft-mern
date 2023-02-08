073976dfc17d6cd464c87ee44aa69d79daa3f9c9
Lucas <lucasabbona0@gmail.com>
2023-02-08 14:52:09 -0300

    ElecciondeColaborador-View

--- app/src/pages/Eleccion.jsx
+++ app/src/pages/Eleccion.jsx
@@ -1,6 +1,6 @@
 import { Navbar } from '../components/shared/Navbar'
 import { Footer } from '../components/shared/Footer'
-import Matchcolaborator from '../components/matchcolaborador/matchcolaborator'
+import { Matchcolaborator } from '../components/matchcolaborador/matchcolaborator'
 
 export const Eleccion = () => {
   return (
