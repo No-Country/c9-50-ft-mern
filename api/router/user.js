2485a5b22e2b887e1e0040af6405f14a930b43fc
Ezequiel Antonio Soto <Ezequiel_soto_f4@hotmail.com>
2023-02-08 16:03:11 -0300

    Created path post/recover to recover password

--- api/router/user.js
+++ api/router/user.js
@@ -1,12 +1,13 @@
 const { Router } = require('express')
 const validatorHandle = require('../middlewares/validatorHandler')
-const { loginUserSchema, registerUserSchema } = require('../libs/zod/user.schema.js')
-const { loginUser, registerUser } = require('../controller/auth.controller')
+const { loginUserSchema, registerUserSchema, recoverPasswordSchema } = require('../libs/zod/user.schema.js')
+const { loginUser, registerUser, recoverPassword } = require('../controller/auth.controller')
 
 const router = Router()
 
 // router.post('/register/role', validatorHandle(registerRoleSchema, 'body'), registerRole)
 router.post('/register', validatorHandle(registerUserSchema, 'body'), registerUser)
 router.post('/login', validatorHandle(loginUserSchema, 'body'), loginUser)
+router.post("/recover", validatorHandle(recoverPasswordSchema, 'body'), recoverPassword)
 
 module.exports = router
