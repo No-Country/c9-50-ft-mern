382d641b1863b6493791b23ae107feaf89977457
Lucas <lucasabbona0@gmail.com>
2023-02-08 14:00:55 -0300

    Elecciondecolaborador-front

---
+++ app/src/components/matchcolaborador/matchcolaborator.jsx
@@ -0,0 +1,32 @@
+export const Matchcolaborator = () => {
+  return (
+    <>
+    <div>
+        <form action="">
+            <div>
+                <select name="" id="">
+                    <option value=""></option>
+                </select>
+            </div>
+        </form>
+        <div className="w-4/5">
+            <p>Resultados Encontrados</p>
+        </div>
+        <div className="flex flex-row justify-between items-center w-4/5 h-auto py-10">
+            <div>
+            <div className="flex flex-row justify-center items-center rounded-full w-40 h-40 bg-black">
+                <img src="" alt="" />
+            </div>
+            <div className="flex flex-col items-center">
+                <h2>Marcela Rodriguez</h2>
+                <p>Profesion</p>
+            </div>
+            </div>
+            <div className="h-full">
+                <button className="w-20 h-4/5 bg-blue-600 text-white">INICIAR CHAT</button>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
