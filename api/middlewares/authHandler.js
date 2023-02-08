1b553900dac0f9694d468bef6d6484c32addb8ee
somosa <CarlosEduardoSomosaMartinez@protonmail.com>
2023-02-08 17:37:51 -0600

    el preciado lin

--- api/middlewares/authHandler.js
+++ api/middlewares/authHandler.js
@@ -3,10 +3,10 @@ const secret = process.env.SECRET
 
 function checkjwt(req, res, next) {
   const jwtByUser = req.headers.authorization || ''
-  const jwt = jwtByUser.split(" ").pop()
-  if(!encode(jwt, secret)){
-    next( new Error('token no valido'))
-  }else{
+  const jwt = jwtByUser.split(' ').pop()
+  if (!encode(jwt, secret)) {
+    next(new Error('token no valido'))
+  } else {
     req.user = jwt
     next()
   }
