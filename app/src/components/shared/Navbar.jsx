c8765bff47f8a330274cab88b361aa53ff0d96a0
sashaariel <sashaariel@live.cl>
2023-02-08 19:27:35 -0300

    fix navbar

--- app/src/components/shared/Navbar.jsx
+++ app/src/components/shared/Navbar.jsx
@@ -1,12 +1,11 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import punto from '../shared/img/Punto.png'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false }
+  { name: 'Nosotros', href: '#', current: false },
+  { name: 'Contacto', href: '#', current: false }
 ]
 
 function classNames(...classes) {
@@ -15,7 +14,7 @@ function classNames(...classes) {
 
 export const Navbar = () => {
   return (
-    <Disclosure as='nav' className='bg-gray-800'>
+    <Disclosure as='nav' className='F2F1EF'>
       {({ open }) => (
         <>
           <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
@@ -32,18 +31,20 @@ export const Navbar = () => {
               <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
                 <div className='flex flex-shrink-0 items-center'>
                   <img
-                    className='block h-8 w-auto lg:hidden'
-                    src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500'
+                    className='block h-4 w-auto lg:hidden'
+                    src={punto}
                     alt='Your Company'
                   />
                   <img
-                    className='hidden h-8 w-auto lg:block'
-                    src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500'
-                    alt='Your Company'
+                    className='hidden h-4 w-auto lg:block'
+                    src={punto}
+                    alt='mettapp'
                   />
                 </div>
                 <div className='hidden sm:ml-6 sm:block'>
-                  <div className='flex space-x-4'>
+                  <div className='flex items-center space-x-4'>
+                    <p>MeetApp</p>
+                    <div className='inset-28'>
                     {navigation.map((item) => (
                       <a
                         key={item.name}
@@ -51,7 +52,7 @@ export const Navbar = () => {
                         className={classNames(
                           item.current
                             ? 'bg-gray-900 text-white'
-                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                            : 'text-black hover:bg-gray-700 hover:text-white',
                           'px-3 py-2 rounded-md text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -59,29 +60,18 @@ export const Navbar = () => {
                         {item.name}
                       </a>
                     ))}
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
-                <button
-                  type='button'
-                  className='rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
-                >
-                  <span className='sr-only'>View notifications</span>
-                  <BellIcon className='h-6 w-6' aria-hidden='true' />
-                </button>
+              <div className='absolute inset-y-0 right-0 flex items-center m-auto sm:static sm:inset-auto sm:ml-6 sm:pr-0 bg-primary bg-primaryHover h-8'>
+                <p className='w-auto h-auto text-xs text-white p-4 ml-3'>
+                    Inicio Sesión
+                </p>
 
                 {/* Profile dropdown */}
                 <Menu as='div' className='relative ml-3'>
                   <div>
-                    <Menu.Button className='flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
-                      <span className='sr-only'>Open user menu</span>
-                      <img
-                        className='h-8 w-8 rounded-full'
-                        src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                        alt=''
-                      />
-                    </Menu.Button>
                   </div>
                   <Transition
                     as={Fragment}
