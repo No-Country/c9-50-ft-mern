1022769988b5703128307c51aae8a1fcecb92363
Lucas <lucasabbona0@gmail.com>
2023-02-15 15:50:22 -0300

    ChatView

--- app/src/components/chat/chat.jsx
+++ app/src/components/chat/chat.jsx
@@ -1,7 +1,9 @@
 import { AiOutlineArrowLeft } from 'react-icons/ai'
 import { useNavigate } from 'react-router-dom'
-/* import { useState } from 'react' */
-
+const personas = [
+  { id: 1, nombre: 'Marcela Rodriguez', last: 'Saludos' },
+  { id: 2, nombre: 'Alejandra Jimenez', last: 'Genial' }
+]
 export const ChatView = () => {
   const navigate = useNavigate()
   return (
@@ -24,34 +26,24 @@ export const ChatView = () => {
         </div>
         <div className='flex flex-col w-full absolute top-24 left-0 h-full bg-neutral-100'>
           {/* Contact 1 */}
-          <div
-            className='flex flex-row items-center space-x-3 pl-4 w-full h-24 border-b-2 cursor-pointer'
-            onClick={() => navigate('/chatting')}
-          >
-            <div className='h-16 w-16 overflow-hidden rounded-full'>
-              <img
-                src='https://previews.123rf.com/images/seventyfour74/seventyfour741708/seventyfour74170800167/83829695-retrato-de-hermosa-mujer-psic%C3%B3loga-con-gafas-posando-con-portapapeles-en-la-oficina-de-terapia-c%C3%B3mod.jpg'
-                alt=''
-              />
+          {personas.map((per) => (
+            <div
+              key={per.id}
+              className='flex flex-row items-center space-x-3 pl-4 w-full h-24 border-b-2 cursor-pointer'
+              onClick={() => navigate('/chatting')}
+            >
+              <div className='h-16 w-16 overflow-hidden rounded-full'>
+                <img
+                  src='https://previews.123rf.com/images/seventyfour74/seventyfour741708/seventyfour74170800167/83829695-retrato-de-hermosa-mujer-psic%C3%B3loga-con-gafas-posando-con-portapapeles-en-la-oficina-de-terapia-c%C3%B3mod.jpg'
+                  alt=''
+                />
+              </div>
+              <div className='flex-col'>
+                <h2 className=' text-sm sm:text-md md:text-md font-semibold'>{per.nombre}</h2>
+                <p className='text-sm'>{per.last}</p>
+              </div>
             </div>
-            <div className='flex-col'>
-              <h2 className=' text-sm sm:text-md md:text-md font-semibold'>Marcela Rodriguez</h2>
-              <p className='text-sm'>Last Message</p>
-            </div>
-          </div>
-          {/* Contact 2 */}
-          <div className='flex flex-row items-center space-x-3 pl-4 w-full h-24 border-b-2 cursor-pointer' onClick={() => navigate('/chatting')}>
-            <div className='h-16 w-16 overflow-hidden rounded-full'>
-              <img
-                src='https://previews.123rf.com/images/seventyfour74/seventyfour741708/seventyfour74170800167/83829695-retrato-de-hermosa-mujer-psic%C3%B3loga-con-gafas-posando-con-portapapeles-en-la-oficina-de-terapia-c%C3%B3mod.jpg'
-                alt=''
-              />
-            </div>
-            <div className='flex-col'>
-              <h2 className=' text-sm sm:text-md md:text-md font-semibold'>Marcela Rodriguez II</h2>
-              <p className='text-sm'>Last Message</p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
