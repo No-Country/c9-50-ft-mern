7e503debfe82f7c7e131da5078f2613ac2ac96d3
Cristian Apablaza <nehuenortega@gmail.com>
2023-02-17 14:34:35 -0300

    Add populate messages in chats

--- api/controller/chat.controller.js
+++ api/controller/chat.controller.js
@@ -35,7 +35,7 @@ const chatById = async (req, res, next) => {
   try {
     const { chatId } = req.params
     const roomChat = await findChatByChatId(chatId)
-    success(200, res, { message: roomChat.message, payload: roomChat.id })
+    success(200, res, { message: roomChat.message, payload: roomChat.data })
   } catch (error) {
     next(error)
   }
