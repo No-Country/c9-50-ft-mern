5d701f2bd4f473e81046b3e9a941251b4ede0531
JoseLuria <jos02-log18@hotmail.com>
2023-02-27 11:38:37 -0600

    fixed container styles

--- app/src/components/chat/chatModal.jsx
+++ app/src/components/chat/chatModal.jsx
@@ -42,67 +42,63 @@ export const Modal = () => {
 
   return (
     <>
-      <div className='w-full grow flex flex-col pb-12'>
-        <div className='flex flex-row right-0 w-full p bg-slate-500'>
-          <div
-            className='cursor-pointer h-full flex flex-row items-center ml-5 text-white text-xl'
-            onClick={() => navigate('/chat')}
-          >
-            <AiOutlineArrowLeft />
-          </div>
-          <div className='flex flex-row w-full h-24 pl-10 items-center space-x-3'>
-            <div className='flex items-center w-20 h-20 rounded-full overflow-hidden'>
-              <img
-                src='https://previews.123rf.com/images/seventyfour74/seventyfour741708/seventyfour74170800167/83829695-retrato-de-hermosa-mujer-psic%C3%B3loga-con-gafas-posando-con-portapapeles-en-la-oficina-de-terapia-c%C3%B3mod.jpg'
-                alt=''
-              />
-            </div>
-            <div className='flex flex-col items-start justify-center'>
-              <h2 className='text-white text-lg font-light'>
-                {role === 'PATIENT'
-                  ? activeChat?.infoInChat.users[0].name
-                  : activeChat?.infoInChat.users[1].name}
-              </h2>
-              <p className='text-white font-medium text-sm'>Psicologa</p>
-            </div>
-          </div>
-          <div className='mr-10 mt-4'>
-            <p className='text-white text-4xl cursor-pointer'>...</p>
-          </div>
-        </div>
-        <div className='flex flex-col grow w-full px-5 overflow-hidden py-5'>
-          <div className='h-full'>
-            <div className='overflow-auto'>
-              {messages.map(({ _id, content }) => (
-                <div className='max-w-xs bg-neutral-200 px-5 py-3 rounded-lg' key={_id}>
-                  {content}
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-        <form
-          className='w-full flex flex-row justify-center bottom-10 gap-4'
-          onSubmit={handleSubmit(onSubmit)}
+      <div className='flex flex-row right-0 w-full p bg-slate-500'>
+        <div
+          className='cursor-pointer h-full flex flex-row items-center ml-5 text-white text-xl'
+          onClick={() => navigate('/chat')}
         >
-          <input
-            name='chatMessage'
-            type='text'
-            placeholder='Escribe un mensaje'
-            className='w-3/4 p-4 rounded-lg border-none outline-none bg-neutral-200
-                '
-            {...register('content', { required: true, minLength: 1 })}
-          />
-
-          <button
-            type='submit'
-            className='bg-primary py-5 px-6 hover:bg-primaryHover rounded-full font-bold text-white'
-            disabled={!!errors.chatMessage?.message}
-          >
-            <AiOutlineArrowRight />
-          </button>
-        </form>
+          <AiOutlineArrowLeft />
+        </div>
+        <div className='flex flex-row w-full h-24 pl-10 items-center space-x-3'>
+          <div className='flex items-center w-20 h-20 rounded-full overflow-hidden'>
+            <img
+              src='https://previews.123rf.com/images/seventyfour74/seventyfour741708/seventyfour74170800167/83829695-retrato-de-hermosa-mujer-psic%C3%B3loga-con-gafas-posando-con-portapapeles-en-la-oficina-de-terapia-c%C3%B3mod.jpg'
+              alt=''
+            />
+          </div>
+          <div className='flex flex-col items-start justify-center'>
+            <h2 className='text-white text-lg font-light'>
+              {role === 'PATIENT'
+                ? activeChat?.infoInChat.users[0].name
+                : activeChat?.infoInChat.users[1].name}
+            </h2>
+            <p className='text-white font-medium text-sm'>Psicologa</p>
+          </div>
+        </div>
+        <div className='mr-10 mt-4'>
+          <p className='text-white text-4xl cursor-pointer'>...</p>
+        </div>
       </div>
+      <div className='grow w-full px-5 overflow-hidden py-5'>
+        <div className='h-full w-full overflow-y-auto overflow-x-hidden flex flex-col gap-4'>
+          {messages.map(({ _id, content }) => (
+            <div className='w-[80%] max-w-lg bg-neutral-200 px-5 py-3 rounded-lg' key={_id}>
+              {content}
+            </div>
+          ))}
+        </div>
+      </div>
+      <form
+        className='w-full flex flex-row justify-center bottom-10 gap-4 my-3'
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <input
+          name='chatMessage'
+          type='text'
+          placeholder='Escribe un mensaje'
+          className='w-3/4 p-4 rounded-lg border-none outline-none bg-neutral-200
+                '
+          {...register('content', { required: true, minLength: 1 })}
+        />
+
+        <button
+          type='submit'
+          className='bg-primary py-5 px-6 hover:bg-primaryHover rounded-full font-bold text-white'
+          disabled={!!errors.chatMessage?.message}
+        >
+          <AiOutlineArrowRight />
+        </button>
+      </form>
     </>
   )
 }
