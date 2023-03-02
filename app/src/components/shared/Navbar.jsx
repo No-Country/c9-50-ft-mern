9336790ed653c84159a7c1f10453814d295e267e
agustinlopez23 <agustinlopez223@gmail.com>
2023-03-02 13:58:22 -0300

    first

--- app/src/components/shared/Navbar.jsx
+++ app/src/components/shared/Navbar.jsx
@@ -26,58 +26,59 @@ export const Navbar = () => {
   }
   return (
     <Disclosure as='nav' className='F2F1EF'>
-      {({ open }) => {
-        return (
-          <>
-            <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
-              <div className='relative flex h-16 items-center justify-between'>
-                <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
-                  <Disclosure.Button className='inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
-                    <span className='sr-only'>Open main menu</span>
-                    {open ? (
+      {({ open }) => (
+        <>
+          <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
+            <div className='relative flex h-16 items-center justify-between'>
+              <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
+                <Disclosure.Button className='inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+                  <span className='sr-only'>Open main menu</span>
+                  {open
+                    ? (
                       <XMarkIcon className='block h-6 w-6' aria-hidden='true' />
-                    ) : (
+                      )
+                    : (
                       <Bars3Icon className='block h-6 w-6' aria-hidden='true' />
-                    )}
-                  </Disclosure.Button>
+                      )}
+                </Disclosure.Button>
+              </div>
+              <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
+                <div className='flex flex-shrink-0 items-center'>
+                  <img className='hidden h-4 w-auto sm:block' src={punto} alt='mettapp' />
                 </div>
-                <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
-                  <div className='flex flex-shrink-0 items-center'>
-                    <img className='hidden h-4 w-auto sm:block' src={punto} alt='mettapp' />
-                  </div>
-                  <div className='hidden sm:ml-6 sm:block'>
-                    <div className='flex items-center space-x-4'>
-                      <Link to='/'>MeetApp</Link>
-                      <div className='inset-28'>
-                        {navigation.map((item) => (
-                          <Link
-                            key={item.name}
-                            to={item.href}
-                            className={classNames(
-                              item.current
-                                ? 'bg-gray-900 text-white'
-                                : 'text-black hover:bg-gray-700 hover:text-white',
-                              'px-3 py-2 rounded-md text-sm font-medium'
-                            )}
-                            aria-current={item.current ? 'page' : undefined}
-                          >
-                            {item.name}
-                          </Link>
-                        ))}
-                      </div>
+                <div className='hidden sm:ml-6 sm:block'>
+                  <div className='flex items-center space-x-4'>
+                    <Link to='/'>MeetApp</Link>
+                    <div className='inset-28'>
+                      {navigation.map((item) => (
+                        <Link
+                          key={item.name}
+                          to={item.href}
+                          className={classNames(
+                            item.current
+                              ? 'bg-gray-900 text-white'
+                              : 'text-black hover:bg-gray-700 hover:text-white',
+                            'px-3 py-2 rounded-md text-sm font-medium'
+                          )}
+                          aria-current={item.current ? 'page' : undefined}
+                        >
+                          {item.name}
+                        </Link>
+                      ))}
                     </div>
                   </div>
                 </div>
-                <div className='absolute inset-y-0 right-0 flex items-center m-auto sm:static sm:inset-auto sm:ml-6 sm:pr-0 bg-primary hover:bg-primaryHover h-8 shadow-md'>
-                  {status === 'authenticated' ? (
+              </div>
+              <div className='absolute inset-y-0 right-0 flex items-center m-auto sm:static sm:inset-auto sm:ml-6 sm:pr-0 bg-primary hover:bg-primaryHover h-8 shadow-md'>
+                {status === 'authenticated'
+                  ? (
                     <Menu as='div' className='relative ml-3'>
                       <div>
                         <Menu.Button className='inline-flex w-full justify-center rounded-md bg-transparent bg-opacity-20 px-3 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75'>
                           <p className='w-auto h-auto text-xs text-white p-3 ml-1'>{name}</p>
                           <ChevronDownIcon
                             className='ml-2 mt-3 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100'
-                            aria-hidden='true'
-                          />
+                            aria-hidden='true' />
                         </Menu.Button>
                       </div>
                       <Transition
@@ -178,38 +179,38 @@ export const Navbar = () => {
                         </Menu.Items>
                       </Transition>
                     </Menu>
-                  ) : (
+                    )
+                  : (
                     <Link className='w-auto h-auto text-xs text-white p-4 ml-3' to='/login'>
                       Inicio Sesión
                     </Link>
-                  )}
-                </div>
+                    )}
               </div>
             </div>
+          </div>
 
-            <Disclosure.Panel className='sm:hidden'>
-              <div className='space-y-1 px-2 pt-2 pb-3'>
-                {navigation.map((item) => (
-                  <Disclosure.Button
-                    key={item.name}
-                    as={Link}
-                    to={item.href}
-                    className={classNames(
-                      item.current
-                        ? 'bg-gray-900 text-white'
-                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'block px-3 py-2 rounded-md text-base font-medium'
-                    )}
-                    aria-current={item.current ? 'page' : undefined}
-                  >
-                    {item.name}
-                  </Disclosure.Button>
-                ))}
-              </div>
-            </Disclosure.Panel>
-          </>
-        )
-      }}
+          <Disclosure.Panel className='sm:hidden'>
+            <div className='space-y-1 px-2 pt-2 pb-3'>
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as={Link}
+                  to={item.href}
+                  className={classNames(
+                    item.current
+                      ? 'bg-gray-900 text-white'
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
     </Disclosure>
   )
 }
