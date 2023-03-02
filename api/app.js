08231cec12dbf631e69a8c3597280c3296ba4c2d
JoseLuria <jos02-log18@hotmail.com>
2023-03-02 13:21:49 -0600

    changes socket server

--- api/app.js
+++ api/app.js
@@ -1,10 +1,11 @@
 require('dotenv').config({})
-const { Server } = require('socket.io')
+const socketIo = require('socket.io')
 const Socket = require('./libs/socket')
 const express = require('express')
 const cors = require('cors')
 const router = require('./router')
 const databaseConnect = require('./db')
+const http = require('http')
 const {
   logError,
   handlerErrorZod,
@@ -20,7 +21,9 @@ const MONGO_URL = process.env.MONGO_URL
 
 const app = express()
 
-const io = new Server(3002)
+const server = http.createServer(app)
+
+const io = socketIo(server)
 
 app.use(express.json())
 app.use(cors())
@@ -40,6 +43,8 @@ if (isProduction) {
 }
 
 new Socket(io)
+
+io.listen(3002)
 
 app.use(logError)
 app.use(handlerErrorZod)
