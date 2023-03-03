9141427338eedb3c175b6f8157a585c0ba193938
JoseLuria <jos02-log18@hotmail.com>
2023-03-02 21:48:18 -0600

    added env to project

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,20 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
+    },
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3001',
+          changeOrigin: true,
+          secure: true
+        }
       }
+    },
+    define: {
+      RAILWAY_STATIC_URL: env.RAILWAY_STATIC_URL
     }
   }
 })
